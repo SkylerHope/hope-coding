3fb4c54777923809563903da7fc3c29e2c07417e
SkylerHope <elpidakouro13@gmail.com>
2024-03-04 12:01:25 +0200

    added first article to coding array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import './HomePage.css';
+import CodingPage from './pages/CodingPage';
+import LinuxPage from './pages/LinuxPage';
+
+const codingArticles = [
+  {
+    id: 1,
+    date: "04/03/2024",
+    title: "Coding article",
+    category: "Programming",
+    imageUrl: "https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg",
+    content: "This is a paragraph for the coding article",
+    linkUrl: "https://www.freepik.com/",
+    linkName: "Freepik"
+  }
+]
 
 function App() {
   return (
