a69febb26d3b3fa123b22fa8e5abb5ddc3db7166
SkylerHope <elpidakouro13@gmail.com>
2024-03-04 12:43:37 +0200

    created about me page

---
+++ src/pages/AboutMePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutMePage = () => {
+    return (
+        <div>
+            <img src="https://files.mastodon.social/accounts/avatars/111/918/013/232/891/984/original/3e48dcc21a18d33c.jpeg"/>
+            <h1>About Me</h1>
+            <h3>This is the about me paragraph</h3>
+            <div>
+                <a href="https://github.com/SkylerHope">GitHub</a>
+                <a href="https://mastodon.social/@Skyler_Sh">Mastodon</a>
+                <a href="https://twitter.com/SkylerHopeSh">Twitter</a>
+                <a href="https://dev.to/skylerhope">Dev</a>
+            </div>
+        </div>
+    );
+};
+
+export default AboutMePage;
