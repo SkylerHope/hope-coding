3bece86085310d4139715bf0146f7dbd5ea94963
SkylerHope <elpidakouro13@gmail.com>
2024-03-04 13:00:44 +0200

    linking pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './GlobalBackground.css';
@@ -7,6 +8,34 @@ import ToolsPage from './pages/ToolsPage';
 import AboutMePage from './pages/AboutMePage';
 
 function App() {
+  const pathname = window.location.pathname;
+  const [pageContent, setPageContent] = useState(null);
+
+  useEffect(() => {
+    let newPageContent;
+
+    if(pathname === '/coding') {
+      document.title = 'Coding journey - Hope Coding';
+      newPageContent = <CodingPage codingArticles={codingArticles}/>
+    } else if(pathname === '/linux') {
+      document.title = 'Linux adventures - Hope Coding';
+      newPageContent = <LinuxPage linuxArticles={linuxArticles}/>
+    } else if(pathname === '/tools') {
+      document.title = 'Free tools and programs - Hope Coding';
+      newPageContent = <ToolsPage toolsContent={toolsContent}/>
+    } else if(pathname ==='/about') {
+      document.title = 'About SkylerHope';
+      newPageContent = <AboutMePage/>
+    } else {
+      newPageContent = (
+        <div>
+          <h1>Latest articles</h1>
+        </div>
+      );
+    }
+    setPageContent(newPageContent);
+  }, [pathname]);
+
   return (
     <div className="App">
       <header className="App-header">
--- src/pages/ToolsPage.js
+++ src/pages/ToolsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const ToolsPage = () => {
+const ToolsPage = ({ toolsContent }) => {
     const toolsContent = [
         {
             id: 1,
