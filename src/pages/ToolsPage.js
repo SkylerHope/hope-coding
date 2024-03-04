fb28ffc7c6f0e3dd910aa7b0aee2eb99cf63b886
SkylerHope <elpidakouro13@gmail.com>
2024-03-04 12:34:02 +0200

    created tools page template

---
+++ src/pages/ToolsPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+const ToolsPage = () => {
+    const toolsContent = [
+        {
+            id: 1,
+            name: "exiftool",
+            description: "Display and edit file metadata",
+            toolUrl: "https://exiftool.org/",
+            toolUrlName: "exiftool.org"
+        }
+    ];
+
+    return (
+        <div>
+            <h1><i>Here's a list of free tools and programs:</i></h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>URL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {toolsContent.map(tools => (
+                        <tr key={tools.id}>
+                            <td>{tools.name}</td>
+                            <td>{tools.description}</td>
+                            <td><a href={tools.toolUrl}>{tools.toolUrlName}</a></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default ToolsPage;
