b166be9a81cff16e63666e090f7b7c0026e93f49
SkylerHope <elpidakouro13@gmail.com>
2024-03-04 10:59:15 +0200

    first change

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This website is still in development</p>
+        <h6>Image provided by <a href='https://www.flaticon.com/'>FLATICON</a></h6>
       </header>
     </div>
   );
