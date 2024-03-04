66dc199bd206eb6db716a8d348ccb69d904867a4
SkylerHope <elpidakouro13@gmail.com>
2024-03-04 11:31:02 +0200

    removed in development text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This website is still in development</p>
-        <h6>Image provided by <a href='https://www.flaticon.com/'>FLATICON</a></h6>
       </header>
     </div>
   );
