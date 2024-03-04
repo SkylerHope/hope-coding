f8f8e8381667a5f9b4b155e7cc9d55229bd293f7
SkylerHope <elpidakouro13@gmail.com>
2024-03-04 11:44:25 +0200

    created coding page template

---
+++ src/pages/CodingPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const CodingPage = ({ codingArticles }) => {
+    return (
+        <div>
+            <h1>Coding related articles</h1>
+            {codingArticles.map(article => (
+                <div key={article.id}>
+                    <table>
+                        <tr>
+                            <th>{article.date}</th>
+                            <th>{article.title}</th>
+                            <th>{article.category}</th>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>{article.content}</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td><a href={article.linkUrl}>{article.linkName}</a></td>
+                        </tr>
+                    </table>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default CodingPage;
