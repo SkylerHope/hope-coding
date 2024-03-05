8ab73a6a3cad2df5b1ed78228017f12cda07a833
SkylerHope <elpidakouro13@gmail.com>
2024-03-05 13:15:18 +0200

    customizing style of latest articles on homepage

--- src/App.js
+++ src/App.js
@@ -76,13 +76,13 @@ function App() {
         <div>
           <h1 className='latest-articles-text'>Latest articles</h1>
           <div className='latest-articles'>
-            <a href='https://hope-coding.netlify.app/coding'>
-              <img src='https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg' className='article-image'/>
+            <a href='https://hope-coding.netlify.app/coding' className='latest-item'>
+              <img src='https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg' className='latest-articles-image' alt='Coding article'/>
               <br/>
               <h4>Coding article</h4>
             </a>
-            <a href='https://hope-coding.netlify.app/linux'>
-              <img src='https://pakhotin.org/wp-content/uploads/2023/07/53113-106400-Linux-xl-1024x576.jpg' className='article-image'/>
+            <a href='https://hope-coding.netlify.app/linux' className='latest-item'>
+              <img src='https://pakhotin.org/wp-content/uploads/2023/07/53113-106400-Linux-xl-1024x576.jpg' className='latest-articles-image' alt='Linux article'/>
               <br/>
               <h4>Linux article</h4>
             </a>
