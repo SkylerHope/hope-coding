7b8561fd1d72a6526cce6d0892538699d68cbbee
SkylerHope <elpidakouro13@gmail.com>
2024-03-05 12:08:50 +0200

    fixed all warnings and errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './GlobalBackground.css';
 import Navbar from './Navbar';
@@ -12,36 +11,36 @@ function App() {
   const pathname = window.location.pathname;
   const [pageContent, setPageContent] = useState(null);
 
-  const linuxArticles = [
-    {
-      id: 1,
-      date: "04/03/2024",
-      title: "Linux article",
-      category: "Operating systems",
-      imageUrl: "https://pakhotin.org/wp-content/uploads/2023/07/53113-106400-Linux-xl-1024x576.jpg",
-      imageName: "Linux image",
-      content: "This is a paragraph for the linux article",
-      linkUrl: "https://www.linux.org/",
-      linkName: "Linux"
-    }
-  ];
-
-  const codingArticles = [
-    {
-      id: 1,
-      date: "04/03/2024",
-      title: "Coding article",
-      category: "Programming",
-      imageUrl: "https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg",
-      imageName: "Coding image",
-      content: "This is a paragraph for the coding article",
-      linkUrl: "https://www.freepik.com/",
-      linkName: "Freepik"
-    }
-  ];
-
   useEffect(() => {
     let newPageContent;
+
+    const codingArticles = [
+      {
+        id: 1,
+        date: "04/03/2024",
+        title: "Coding article",
+        category: "Programming",
+        imageUrl: "https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg",
+        imageName: "Coding image",
+        content: "This is a paragraph for the coding article",
+        linkUrl: "https://www.freepik.com/",
+        linkName: "Freepik"
+      }
+    ];
+
+    const linuxArticles = [
+      {
+        id: 1,
+        date: "04/03/2024",
+        title: "Linux article",
+        category: "Operating systems",
+        imageUrl: "https://pakhotin.org/wp-content/uploads/2023/07/53113-106400-Linux-xl-1024x576.jpg",
+        imageName: "Linux image",
+        content: "This is a paragraph for the linux article",
+        linkUrl: "https://www.linux.org/",
+        linkName: "Linux"
+      }
+    ];
 
     if(pathname === '/coding') {
       document.title = 'Coding journey - Hope Coding';
@@ -68,6 +67,7 @@ function App() {
   return (
     <div>
       <Navbar/>
+      {pageContent}
     </div>
   );
 }
