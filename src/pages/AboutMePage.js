9c6fa023166bf95ec9c4054c4dad9b7cdced9c07
SkylerHope <elpidakouro13@gmail.com>
2024-03-06 13:44:16 +0200

    added spacing between social links

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -7,11 +7,11 @@ const AboutMePage = () => {
             <img src="https://files.mastodon.social/accounts/avatars/111/918/013/232/891/984/original/3e48dcc21a18d33c.jpeg" alt="Skyler Avatar" className="avatar"/>
             <h1>About Me</h1>
             <h3>This is the about me paragraph</h3>
-            <div>
-                <a href="https://github.com/SkylerHope">GitHub</a>
-                <a href="https://mastodon.social/@Skyler_Sh">Mastodon</a>
-                <a href="https://twitter.com/SkylerHopeSh">Twitter</a>
-                <a href="https://dev.to/skylerhope">Dev</a>
+            <div className="social-div">
+                <a href="https://github.com/SkylerHope" className="social-item">GitHub</a>
+                <a href="https://mastodon.social/@Skyler_Sh" className="social-item">Mastodon</a>
+                <a href="https://twitter.com/SkylerHopeSh" className="social-item">Twitter</a>
+                <a href="https://dev.to/skylerhope" className="social-item">Dev</a>
             </div>
         </div>
     );
