b4c7feb67b3cc954f10b86703df0a2af76c3a50f
SkylerHope <elpidakouro13@gmail.com>
2024-03-07 13:09:52 +0200

    added coding article

--- src/App.js
+++ src/App.js
@@ -17,26 +17,15 @@ function App() {
 
     const codingArticles = [
       {
-        id: 2,
-        date: "07/03/2024",
-        title: "New coding article",
-        category: "Programming",
-        imageUrl: "https://www.computersciencedegreehub.com/wp-content/uploads/2023/02/shutterstock_535124956-scaled.jpg",
-        imageName: "New coding image",
-        content: "This is a new article related to coding. Its new, fresh and also it is meant to be used for testing with the navbar. Nice. Woooooo",
-        linkUrl: "https://www.computersciencedegreehub.com/",
-        linkName: "Image source"
-      },
-      {
         id: 1,
-        date: "04/03/2024",
-        title: "Coding article",
-        category: "Programming",
-        imageUrl: "https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg",
-        imageName: "Coding image",
-        content: "This is a paragraph for the coding article",
-        linkUrl: "https://www.freepik.com/",
-        linkName: "Freepik"
+        date: "07/03/2024",
+        title: "Join this community of developers",
+        category: "Website",
+        imageUrl: "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/83/96/4f/83964fcd-de07-3829-ad64-c5726e09e5dc/AppIcon-1x_U007emarketing-0-0-0-10-0-0-85-220.png/1200x630wa.png",
+        imageName: "Devto Logo",
+        content: "I recently discovered the dev.to website. So far it seems like a great way for developers to interact with each other, post articles, debate, and connect. I didn't post anything yet but I plan to. My dev.to link is in the About page. This website is worth checking, give it a try!",
+        linkUrl: "https://dev.to/",
+        linkName: "Dev Community"
       }
     ];
 
@@ -87,15 +76,15 @@ function App() {
         <div>
           <h1 className='latest-articles-text'>Latest articles</h1>
           <div className='latest-articles'>
+            <a href='https://hope-coding.netlify.app/coding' className='latest-item'>
+              <img src='https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/83/96/4f/83964fcd-de07-3829-ad64-c5726e09e5dc/AppIcon-1x_U007emarketing-0-0-0-10-0-0-85-220.png/1200x630wa.png' className='latest-articles-image' alt='Coding article'/>
+              <hr/>
+              <h4>Join this community of developers</h4>
+            </a>
             <a href='https://hope-coding.netlify.app/linux' className='latest-item'>
               <img src='https://news.itsfoss.com/content/images/size/w1304/wordpress/2022/05/linus-torvalds-nvidia.jpg' className='latest-articles-image' alt='Linux article'/>
               <hr/>
               <h4>NVIDIA and Linux? I survived a full system upgrade!</h4>
-            </a>
-            <a href='https://hope-coding.netlify.app/coding' className='latest-item'>
-              <img src='https://www.computersciencedegreehub.com/wp-content/uploads/2023/02/shutterstock_535124956-scaled.jpg' className='latest-articles-image' alt='Coding article'/>
-              <hr/>
-              <h4>Coding article</h4>
             </a>
           </div>
         </div>
